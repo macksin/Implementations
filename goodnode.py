1f19cc4d3fd354d9550cc612058698f438882f3c
mashcon <mack.heyha@gmail.com>
2023-01-14 06:31:11 -0300

    bugfix(cuts): Recursive memory cuts

--- goodnode.py
+++ goodnode.py
@@ -9,35 +9,37 @@ def checkDim(dim, data):
     return dim
 
 
-
-
 def insert(data, dim = None, node = None, k = 8):
 
     dim = checkDim(dim, data)
 
-    cut = np.median(data[:, dim])
+    if dim == 0:
+        cuts = np.median(data, axis=0)
+        if hasattr(node, 'father'):
+            node.cuts = cuts
+    else:
+        cuts = node.father.cuts
 
     if node is None:
-        node = Node(data, cut, dim)
+        node = Node(data, cuts, dim, None)
 
-    dataLeft = data[data[:, dim] <= cut]
-    dataRight = data[data[:, dim] > cut]
+    dataLeft = data[data[:, dim] <= node.cuts[node.dim]]
+    dataRight = data[data[:, dim] > node.cuts[node.dim]]
 
+    # Solve issue where we only create complete `Nodes`
     if dataLeft.shape[0] < k:
         return node
     if dataRight.shape[0] < k:
         return node
 
     dimL = checkDim(dim+1, dataLeft)
-    cutL = np.median(dataLeft[:, dimL])
     if node.left is None:
-        node.left = Node(dataLeft, cutL, dimL)
+        node.left = Node(dataLeft, cuts, dimL, node)
         insert(dataLeft, dimL, node.left, k)
 
     dimR = checkDim(dim+1, dataRight)
-    cutR = np.median(dataRight[:, dimR])
     if node.right is None:
-        node.right = Node(dataRight, cutR, dimR)
+        node.right = Node(dataRight, cuts, dimR, node)
         insert(dataRight, dimR, node.right, k)
 
     return node
@@ -45,13 +47,14 @@ def insert(data, dim = None, node = None, k = 8):
 
 class Node:
 
-    def __init__(self, data, cut, dim):
+    def __init__(self, data, cuts, dim, father):
         self.data = data
-        self.cut = cut
+        self.cuts = cuts
         self.dim = dim
         self.left = None
         self.right = None
         self.label = None
+        self.father = father
 
     def labelLeafs(self, label=0):
         if not self.left and not self.right:
@@ -68,10 +71,10 @@ class Node:
         if self.left is None and self.right is None:
             return self.label
 
-        if data[self.dim] <= self.cut:
+        if data[self.dim] <= self.cuts[self.dim]:
             return self.left.search(data)
         
-        if data[self.dim] > self.cut:
+        if data[self.dim] > self.cuts[self.dim]:
             return self.right.search(data)
 
 
@@ -96,14 +99,12 @@ class AdaptivePartitioning(BaseEstimator, TransformerMixin):
 from sklearn.datasets import make_blobs
 import matplotlib.pyplot as plt
 
-X, y = make_blobs(n_samples=1000, centers=3, n_features=2, random_state=0)
+X, y = make_blobs(n_samples=1000, centers=4, cluster_std=0.35, n_features=2, random_state=0)
 
-ap = AdaptivePartitioning(k = 250)
+ap = AdaptivePartitioning(k = 200)
 labels = ap.fit_transform(X)
 
 print(np.bincount(labels))
 
-
 plt.scatter(X[:, 0], X[:, 1], c=labels)
 plt.show()
-
